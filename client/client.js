d6a61b4d7d407fcbbd6866d0387a638d80ccc9a3
BenThorn <brt4557@g.rit.edu>
2018-11-29 18:00:48 -0500

    api hookup

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ const sendAjax = (type, action, data) => {
     console.log(data);
     $.ajax({
       cache: false,
-      type: "POST",
+      type: type,
       url: action,
       data: data,
       dataType: "json",
@@ -17,18 +17,37 @@ const sendAjax = (type, action, data) => {
   };
 
 const handleLogin = (e) => {
+
     e.preventDefault();
 
     if($("#username").val() === '' || $("#password").val() === ''){
         alert('Please fill in all fields');
         return false;
     }
-    sendAjax('POST', "/login", $("#loginForm").serialize());
+    sendAjax("POST", "/login", $("#getForm").serialize());
+};
+
+const handleGet = (e) => {
+  e.preventDefault();
+
+  if($("#username").val() === ''){
+    alert('Please fill in all fields');
+    return false;
+  }
+
+  console.log('go');
+
+  sendAjax("GET", "/getAllStudents", $("#getForm").serialize());
 };
 
 const init = () => {
-    const loginRequest = (e) => handleLogin(e);
-    document.querySelector("#loginForm").addEventListener('submit', handleLogin);
+  //  const loginRequest = (e) => handleLogin(e);
+  //  document.querySelector("#loginForm").addEventListener('submit', loginRequest);
+  $("#username").focus();
+
+  $("#getForm").submit((e) => {
+    handleGet(e);
+  });
 };
 
 window.onload = init;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const url = require('url');
 const query = require('querystring');
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
+const getJSON = require('get-json');
 
 const main = require('./main.js');
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
@@ -11,6 +13,9 @@ const js = fs.readFileSync(`${__dirname}/../client/client.js`);
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) =>{
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -24,41 +29,74 @@ app.get('/client.js', (request, response) =>{
   response.end();
 });
 
-app.post('/login', (request, response) => {
-  const req = request;
-  const res = response;
-  console.log(req.body);
+// app.post('/login', (request, response) => {
+//   const req = request;
+//   const res = response;
+//   console.log(req.body);
   
-  const postData = query.stringify({
-    username: `${req.body.username}`,
-    password: `${req.body.pass}`
-  });
+//   const postData = query.stringify({
+//     username: `${req.body.username}`,
+//     password: `${req.body.pass}`
+//   });
 
-  console.log(postData);
+//   console.log(postData);
 
-  const postOptions = {
-    host: 'backend',
-    port: '80',
-    path: '/login',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData)
+//   const postOptions = {
+//     host: 'backend',
+//     port: '80',
+//     path: '/login',
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Content-Length': Buffer.byteLength(postData)
+//     }
+//   };
+
+//   /**
+//    * For get, append 
+//    */
+
+//   request.on('error', (err) => {
+//     console.dir(err);
+//     res.statusCode = 400;
+//     res.end();
+//   });
+
+//   response.on('data', (chunk) => {
+//     console.log('Response: ', chunk);
+//   });
+
+//   request.write(postData);
+//   request.end();
+// });
+
+app.get('/getAllStudents', (request, response) => {
+  
+  // const getOptions = {
+  //   host: 'http://serenity.ist.rit.edu/~ra7918/330/research_database/api',
+  //   port: '80',
+  //   path: '/user/getAllStudents.php',
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // };
+
+  const url = 'http://serenity.ist.rit.edu/~ra7918/330/research_database/api/user/getAllStudents.php';
+
+  getJSON(url, (error, res) => {
+    if (!error) {
+      const object = {
+        data: res.data,
+      };
+      console.log(res);
+      return response.json({user: 'Ben'});
+    } else {
+      console.log(error);
     }
-  };
-
-  request.on('error', (err) => {
-    console.dir(err);
-    res.statusCode = 400;
-    res.end();
   });
 
-  response.on('data', (chunk) => {
-    console.log('Response: ', chunk);
-  });
-
-  request.write(postData);
-  request.end();
+  
 });
 
 app.listen(port, (err) => {
